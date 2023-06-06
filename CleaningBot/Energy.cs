076219295b2da6a211ca2d265d200ec0fbc6c156
Patrik Sima <psim@ciklum.com>
2023-06-06 21:49:45 +0200

    refactoring

---
+++ CleaningBot/Energy.cs
@@ -0,0 +1,9 @@
+namespace CleaningBot;
+
+public static class Energy
+{
+    public const int Clean = 5;
+    public const int Turn = 1;
+    public const int Back = 3;
+    public const int Advance = 2;
+}
